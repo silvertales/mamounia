1a686651982ad3f8e6a8b43df15aa315ef8fc209
Erfan Taherian <erfantaherian13@gmail.com>
2026-06-05 10:41:46 +0330

    Fix for Vercel deployment

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ app.secret_key = 'ladecima11'   # needed for flash messages
 def home():
     return render_template('index.html')
 
-@app.route('/comment', methods=['POST'])
-def comment():
-    flash('Thank you for your comment! We appreciate your feedback.', 'success')
-    return redirect(url_for('home', _anchor='reserve'))
-
 @app.route('/reserve', methods=['POST'])
 def reserve():
     flash('Your table has been reserved! We look forward to serving you.', 'success')
+    return redirect(url_for('home', _anchor='reserve'))
+
+@app.route('/comment', methods=['POST'])
+def comment():
+    flash('Thank you for your comment! We appreciate your feedback.', 'success')
     return redirect(url_for('home', _anchor='reserve'))
